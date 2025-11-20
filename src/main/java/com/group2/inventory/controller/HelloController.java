1e00be2a4e65fbd96ae64a73d1253347cf3f68ff
orchlonn <ch.orchlon@gmail.com>
2025-11-19 19:21:42 -0800

    hello get api added

---
+++ src/main/java/com/group2/inventory/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.group2.inventory.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World from DexTor API!";
+    }
+}
+
