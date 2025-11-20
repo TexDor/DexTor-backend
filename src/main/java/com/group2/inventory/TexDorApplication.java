6c277056d164d2bba423b817f8e2912f89c0cfbd
orchlonn <ch.orchlon@gmail.com>
2025-11-19 19:14:54 -0800

    backend initialized

---
+++ src/main/java/com/group2/inventory/TexDorApplication.java
@@ -0,0 +1,13 @@
+package com.group2.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TexDorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TexDorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group2/inventory/TexDorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group2.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TexDorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
